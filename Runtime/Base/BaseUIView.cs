5865e6e2c564bd0dcc12efd4f0dd85ef42816b89
NguyenZuy <nguyenduygamedev@gmail.com>
2024-12-05 15:39:33 +0700

    update base UI View

---
+++ Runtime/Base/BaseUIView.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Zuy.Workspace
+{
+    public abstract class BaseUIView : MonoBehaviour
+    {
+        protected virtual void Awake() { }
+        protected virtual void Start() { }
+        protected virtual void Update() { }
+        protected virtual void FixedUpdate() { }
+        protected virtual void LateUpdate() { }
+        protected virtual void OnEnable() { }
+        protected virtual void OnDisable() { }
+        protected virtual void OnDestroy() { }
+        protected virtual void OnApplicationQuit() { }
+    }
+}
