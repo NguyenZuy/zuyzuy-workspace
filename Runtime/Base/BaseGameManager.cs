41a14ccd894937d49b803e26027f6b05d0f41e62
NguyenZuy <nguyenduygamedev@gmail.com>
2024-12-05 16:19:53 +0700

    update

--- Runtime/Base/BaseGameManager.cs
+++ Runtime/Base/BaseGameManager.cs
@@ -1,15 +1,26 @@
 namespace Zuy.Workspace
 {
-    public abstract class BaseGameManager : BaseSingleton<BaseGameManager>
+    public abstract class BaseGameManager<T> : BaseSingleton<T> where T : BaseGameManager<T>
     {
-        protected override void Awake() { }
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
         protected virtual void Start() { }
+
         protected virtual void Update() { }
+
         protected virtual void FixedUpdate() { }
+
         protected virtual void LateUpdate() { }
+
         protected virtual void OnEnable() { }
+
         protected virtual void OnDisable() { }
+
         protected virtual void OnDestroy() { }
+
         protected virtual void OnApplicationQuit() { }
     }
 }
--- Runtime/Base/BaseUIView.cs
+++ Runtime/Base/BaseUIView.cs
@@ -2,16 +2,29 @@ using UnityEngine;
 
 namespace Zuy.Workspace
 {
-    public abstract class BaseUIView : MonoBehaviour
+    public abstract class BaseUIView<T> : MonoBehaviour where T : BaseGameManager<T>
     {
-        protected virtual void Awake() { }
+        protected T m_GameManager;
+
+        protected virtual void Awake()
+        {
+            m_GameManager = BaseGameManager<T>.Instance;
+        }
+
         protected virtual void Start() { }
+
         protected virtual void Update() { }
+
         protected virtual void FixedUpdate() { }
+
         protected virtual void LateUpdate() { }
+
         protected virtual void OnEnable() { }
+
         protected virtual void OnDisable() { }
+
         protected virtual void OnDestroy() { }
+
         protected virtual void OnApplicationQuit() { }
     }
 }
