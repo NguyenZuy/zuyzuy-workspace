25185a976ef8b0aada22e4292a2d252eacc0cb4b
NguyenZuy <nguyenduygamedev@gmail.com>
2025-03-29 17:35:59 +0700

    update

--- Runtime/Utils/ConvertUtils.cs
+++ Runtime/Utils/ConvertUtils.cs
@@ -1,6 +1,22 @@
+using System.Numerics;
+
 namespace ZuyZuy.Workspace
 {
     public static class ConvertUtils
     {
+        public static Vector3 OnlyXY(this Vector3 value)
+        {
+            return new Vector3(value.X, value.Y, 0f);
+        }
+
+        public static Vector3 OnlyXZ(this Vector3 value)
+        {
+            return new Vector3(value.X, 0f, value.Z);
+        }
+
+        public static Vector3 OnlyYZ(this Vector3 value)
+        {
+            return new Vector3(0f, value.Y, value.Z);
+        }
     }
 }
