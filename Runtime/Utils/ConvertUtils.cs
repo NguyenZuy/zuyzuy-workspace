6870862acfe60903c170e5696381b95df529fc3f
NguyenZuy <nguyenduygamedev@gmail.com>
2025-03-30 10:35:50 +0700

    update

--- Runtime/Utils/ConvertUtils.cs
+++ Runtime/Utils/ConvertUtils.cs
@@ -18,5 +18,35 @@ namespace ZuyZuy.Workspace
         {
             return new Vector3(0f, vector.y, vector.z);
         }
+
+        public static Vector3 ToXVector3(this float value)
+        {
+            return new Vector3(value, 0f, 0f);
+        }
+
+        public static Vector3 ToYVector3(this float value)
+        {
+            return new Vector3(0f, value, 0f);
+        }
+
+        public static Vector3 ToZVector3(this float value)
+        {
+            return new Vector3(0f, 0f, value);
+        }
+
+        public static Vector3 ToXVector3(this int value)
+        {
+            return new Vector3(value, 0f, 0f);
+        }
+
+        public static Vector3 ToYVector3(this int value)
+        {
+            return new Vector3(0f, value, 0f);
+        }
+
+        public static Vector3 ToZVector3(this int value)
+        {
+            return new Vector3(0f, 0f, value);
+        }
     }
 }
